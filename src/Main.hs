e6b9e8b3ee259c9e303246c8c233f2d85aa7f2e4
selesdepselesnul <selesdepselesnul@gmail.com>
2017-12-25 06:57:17 +0700

    add split depedency

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,14 @@
 module Main where
+import qualified Data.List.Split as Split
 
 main :: IO ()
 main = do
   putStrLn "hello world"
+
+splitCpusInfo x =
+  Split.splitOn "\n\n" x
+
+readCpuInfo = do
+  x <- readFile "/proc/cpuinfo"
+  let xs = splitCpusInfo x
+  putStrLn $ head xs
