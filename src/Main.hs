963990a663e0fd50b3d7f0708d4ed90e78f89ace
selesdepselesnul <selesdepselesnul@gmail.com>
2017-12-25 08:01:13 +0700

    split per cpu-core

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,38 @@
 module Main where
 import qualified Data.List.Split as Split
+import qualified Data.List as List
 
 main :: IO ()
 main = do
   putStrLn "hello world"
 
-splitCpusInfo x =
-  Split.splitOn "\n\n" x
+
+whereCpuInfo x =
+  List.isInfixOf "model name" x
+  || List.isInfixOf "vendor_id" x
+  || List.isInfixOf "cpu MHz" x
+  || List.isInfixOf "processor" x
+
+splitCpuInfo cpuInfoStr =
+  filter whereCpuInfo
+         $ Split.splitOn "\n" cpuInfoStr
+
+splitCpusInfo bigStr =
+  map splitCpuInfo
+      $ filter
+        (/= "")
+        (Split.splitOn "\n\n" bigStr)
 
 readCpuInfo = do
   x <- readFile "/proc/cpuinfo"
   let xs = splitCpusInfo x
-  putStrLn $ head xs
+  putStrLn (show xs)
+
+
+
+
+
+
+
+
+
