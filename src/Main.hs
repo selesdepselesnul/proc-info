73fed9a8c08a01958b2b20725bcce9c7b0fb59cf
selesdepselesnul <selesdepselesnul@gmail.com>
2017-12-25 09:12:45 +0700

    add type declaration

--- src/Main.hs
+++ src/Main.hs
@@ -7,17 +7,20 @@ main = do
   x <- readFile "/proc/cpuinfo"
   putStrLn $ splitCpusInfo x
 
+whereCpuInfo :: [Char] -> Bool
 whereCpuInfo x =
   List.isInfixOf "model name" x
   || List.isInfixOf "vendor_id" x
   || List.isInfixOf "cpu MHz" x
   || List.isInfixOf "processor" x
 
+splitCpuInfo :: String -> [String]
 splitCpuInfo cpuInfoStr =
   (map (++ "\n")
   (filter whereCpuInfo
           (Split.splitOn "\n" cpuInfoStr)) ) ++ ["\n"]
 
+splitCpusInfo :: String -> String
 splitCpusInfo bigStr =
   "CPU Summary\t:\n\n" ++
   (List.intercalate ""
